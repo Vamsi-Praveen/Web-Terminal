457df3a8b78d559f5d3002374840b861b2b1f1f4
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2024-10-20 09:43:05 +0530

    added node-pty

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
-// const pty = require("node-pty");
+const pty = require("node-pty");
 const os = require("os");
-const { exec } = require("child_process");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,54 +13,37 @@ app.use(express.static(__dirname + "/public"));
 io.on("connection", (socket) => {
   console.log(`New User Connected ${socket.id}`);
 
-  //read the data from the terminal
-  const shell = os.platform() == "win32" ? "powershell.exe" : "bash";
+  // Determine the shell based on OS
+  const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 
-  const prompt = `${os.userInfo().username}@${os.hostname()}:~$ `;
+  // Spawn a new terminal using node-pty
+  const ptyProcess = pty.spawn(shell, [], {
+    name: "xterm-color",
+    cols: 80,
+    rows: 30,
+    cwd: process.env.HOME,
+    env: process.env,
+  });
 
-  // Send the initial prompt when connected
-  socket.emit("output", prompt);
+  // Send the terminal output to the client
+  ptyProcess.on("data", (data) => {
+    socket.emit("output", data);
+  });
 
-  socket.on("prompt", () => socket.emit("output", prompt));
-
-  //   const ptyProcess = pty.spawn(shell, [], {
-  //     name: "xterm-color",
-  //     cols: 80,
-  //     rows: 30,
-  //     cwd: process.env.HOME,
-  //     env: process.env,
-  //   });
-
-  //   //sending data to terminal
-  //   ptyProcess.on("data", (data) => {
-  //     socket.emit("output", data);
-  //   });
-
-  //   //handle input from client and sent to tty
+  // Receive input from the client and send it to the terminal
   socket.on("input", (input) => {
-    if (input === "cls" || input === "clear") {
-      socket.emit("output", prompt);
-    } else {
-      exec(input, (error, stdout, stderr) => {
-        if (error) {
-          socket.emit("output", `Error: ${stderr}`);
-          socket.emit("output", prompt);
-          return;
-        } else {
-          socket.emit("output", stdout);
-        }
-        socket.emit("output", prompt);
-      });
-    }
+    ptyProcess.write(input + "\r");
   });
 
+  // Handle terminal clear command from client
   socket.on("clear", () => {
-    socket.emit("output", prompt);
+    ptyProcess.write("clear\r");
   });
 
+  // Handle client disconnection
   socket.on("disconnect", () => {
     console.log(`User disconnected ${socket.id}`);
-    // ptyProcess.kill();
+    ptyProcess.kill();
   });
 });
 
